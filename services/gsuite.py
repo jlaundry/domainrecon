4f4215ad5319cebe8da8332bc335c94604b6b9a7
Jed Laundry <jlaundry@jlaundry.com>
2023-04-21 04:24:15 +0000

    📬

--- services/gsuite.py
+++ services/gsuite.py
@@ -7,6 +7,15 @@ class ServiceProvider(BaseServiceProvider):
 
     @property
     def is_active(self) -> bool:
+        if self._mx_includes("aspmx.l.google.com"):
+            return True
+
+        if self._mx_includes(r"alt\d.aspmx.l.google.com"):
+            return True
+
+        if self._mx_includes(r"aspmx\d.googlemail.com"):
+            return True
+
         if self._spf_includes(r"_spf.google.com"):
             return True
 
