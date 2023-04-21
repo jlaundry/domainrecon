675898568b067a6304b4870bde63bef16973577c
Jed Laundry <jlaundry@jlaundry.com>
2023-04-21 04:22:56 +0000

    📝

--- services/hubspot.py
+++ services/hubspot.py
@@ -1,7 +1,7 @@
 
 import re
 
-from . import BaseServiceProvider
+from . import BaseServiceProvider, InsufficientDataError
 
 class ServiceProvider(BaseServiceProvider):
     name = "HubSpot"
@@ -24,6 +24,6 @@ class ServiceProvider(BaseServiceProvider):
                 pass
 
         if hs_customer_id is None:
-            raise Exception("Couldn't determine HubSpot customer ID (no SPF info)")
+            raise InsufficientDataError("Couldn't determine HubSpot customer ID (no SPF info)")
 
         return [f"hs1-{hs_customer_id}", f"hs2-{hs_customer_id}"]
